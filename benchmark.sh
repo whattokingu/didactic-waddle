35238aa26c7f05eb203d58942ace45f242a2b22d
Zheng  Weihan <zhengweihan.91@gmail.com>
2016-10-05 00:02:37 +0800

    update bash scripts

--- benchmark.sh
+++ benchmark.sh
@@ -3,12 +3,9 @@
 
 for data in D8 D40
 do
-	for replication in 1 3
+	replication=3
+	for numClients in 10 20 40	
 	do
-		for numClients in 10 20 40
-		do
-			echo "Running benchmark for $data with keyspace replication: $replication and numClients: $numClients"
-			./start.sh $data $numClients $replication > $data-r$replication-$numClients.txt ;
-		done
+		echo "Running benchmark for $data with keyspace replication: $replication and numClients: $numClients"			./start.sh $data $numClients $replication > $data-r$replication-$numClients.txt ;
 	done
 done
