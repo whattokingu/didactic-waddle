ca07c6888b96d4d32f938d6974b5f66dd2d87ce4
Zheng  Weihan <zhengweihan.91@gmail.com>
2016-10-12 19:07:51 +0800

    remove load_data from start shell script

--- start.sh
+++ start.sh
@@ -5,19 +5,11 @@ then
 	echo "wrong number of arguments. Correct call: start.sh <dirname> <numclients> <replicationfactor> <logfile>"
 else
 
-	datadirname=$1-data
 	xactdirname=$1-xact-revised-b
 	numclients=$2
-	replication=$3
-	logfile=$4
-	echo "data directory: " $datadirname
+	logfile=$3
 	echo "xact directory: " $xactdirname
 	echo "starting drivers with number of clients: " $numclients
-	echo "replication factor: " $replication
-	echo "changing keyspace replication to $replication on cassandra node"
-	cqlsh -e "alter keyspace cs4224 with replication = {'class': 'SimpleStrategy', replication_factor' : $replication}"
-	echo "loading directory"
-	python2.7 load_data.py $datadirname ;
 
 	for i in $(seq 0 $(($numclients-2))); do
 		echo "Starting client " $i
