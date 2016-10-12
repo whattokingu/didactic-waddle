e7d2aee47a50bab8c26b36cdb5ec86ecda6e067e
Zheng  Weihan <zhengweihan.91@gmail.com>
2016-10-12 19:18:00 +0800

    update shell script for distributed benchmarking.

--- start.sh
+++ start.sh
@@ -2,21 +2,23 @@
 
 if [ "$#" -ne 4 ];
 then
-	echo "wrong number of arguments. Correct call: start.sh <dirname> <numclients> <replicationfactor> <logfile>"
+	echo "wrong number of arguments. Correct call: start.sh <dirname> <numclients> <offset> <logfile>"
 else
 
 	xactdirname=$1-xact-revised-b
 	numclients=$2
-	logfile=$3
+	offset=$3
+	logfile=$4
 	echo "xact directory: " $xactdirname
 	echo "starting drivers with number of clients: " $numclients
+	echo "xact file offset: " $offset
 
 	for i in $(seq 0 $(($numclients-2))); do
-		echo "Starting client " $i
-		python2.7 driver.py $xactdirname $i $logfile&
+		echo "Starting client " $(($i + $offset))
+		python2.7 driver.py $xactdirname $(($i + $offset)) $logfile&
 	done
-	echo "Starting client " $(($numclients-1))
-	python2.7 driver.py $xactdirname $(($numclients-1)) $logfile ;
+	echo "Starting client " $(($numclients-1+$offset))
+	python2.7 driver.py $xactdirname $(($numclients-1 + $offset)) $logfile ;
 	wait $!
 	sleep 5
 fi
